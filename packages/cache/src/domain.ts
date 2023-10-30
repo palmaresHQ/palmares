543c5e75a8c8cf8d9b0d59b58ddb4d176e35168f
anti-duhring <mateus.limavn@gmail.com>
2023-10-30 14:16:12 -0300

    feat: setup cache package

--- packages/cache/src/domain.ts
+++ packages/cache/src/domain.ts
@@ -1,8 +1,4 @@
 import { domain } from '@palmares/core';
 
 //@ts-ignore
-export default domain('@palmares/cache', __dirname || import.meta.url, {
-  load: async(settings) => {
-
-  },
-});
+export default domain('@palmares/cache', __dirname || import.meta.url, undefined);
