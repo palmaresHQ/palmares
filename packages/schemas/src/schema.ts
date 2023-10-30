e6eb3d99ee555ee9143540920bf7ab9811eb3f39
Nicolas Melo <nicolasmelo@Nicolass-MacBook-Pro.local>
2023-10-30 13:38:40 -0300

    stated working on schemas validation for validating the data that comes from the request and changing the data we send to clients

---
+++ packages/schemas/src/schema.ts
@@ -0,0 +1,47 @@
+export class Schema<
+  TDefinitions,
+  TType extends {
+    input: any;
+    output: any;
+  } = {
+    input: any;
+    output: any;
+  },
+> {
+  __validationSchema: any;
+  __toRepresentation!: (value: TType['output']) => TType['output'];
+  __toInternal!: (value: TType['input']) => TType['input'];
+
+  constructor(validationSchema: any) {
+    this.__validationSchema = validationSchema;
+  }
+
+  toRepresentation<TOutput>(toRepresentationCallback: (value: TType['output']) => TOutput) {
+    this.__toRepresentation = toRepresentationCallback;
+    return this as unknown as Schema<TDefinitions, { input: TType['input']; output: TOutput }>;
+  }
+
+  toInternal<TInput>(toInternalCallback: (value: TType['input']) => TInput) {
+    this.__toInternal = toInternalCallback;
+    return this as unknown as Schema<TDefinitions, { input: TInput; output: TType['output'] }>;
+  }
+
+  static new<TDefinitions, TType extends { input: any; output: any }>(
+    definitions: TDefinitions
+  ): Schema<TDefinitions, TType> {
+    return new Schema<TDefinitions, TType>(undefined);
+  }
+}
+
+const teste = Schema.new({}).toRepresentation((value) => 'teste' as const);
+
+export class NumberSchema<
+  TDefinitions,
+  TType extends {
+    input: number | bigint;
+    output: number | bigint;
+  } = {
+    input: number | bigint;
+    output: number | bigint;
+  },
+> extends Schema<TDefinitions, TType> {}
