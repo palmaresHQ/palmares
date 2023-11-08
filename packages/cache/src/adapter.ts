73b0f1943c99877e2994c7c3d1007fb0671ca4e3
anti-duhring <mateus.limavn@gmail.com>
2023-11-07 21:27:30 -0300

    feat: Add NotImplemented exception on CacheAdapter

--- packages/cache/src/adapter.ts
+++ packages/cache/src/adapter.ts
@@ -1,17 +1,31 @@
+import { CacheAdapterNotImplementedError } from "./exceptions"
+
 export default class CacheAdapter {
   static new(_cacheAdapterConstructor: typeof CacheAdapter, args: any): () => Promise<CacheAdapter> {
-    throw new Error('Method not implemented')
+    throw new CacheAdapterNotImplementedError({
+      className: CacheAdapter.name,
+      functionName: 'new'
+    })
   }
 
   async get(_key: string): Promise<any> {
-    throw new Error('Method not implemented')
+    throw new CacheAdapterNotImplementedError({
+      className: CacheAdapter.name,
+      functionName: 'get'
+    })
   }
 
   async set(_key: string, _value: any) {
-    throw new Error('Method not implemented')
+    throw new CacheAdapterNotImplementedError({
+      className: CacheAdapter.name,
+      functionName: 'set'
+    })
   }
 
   async remove(_key: string) {
-    throw new Error('Method not implemented')
+    throw new CacheAdapterNotImplementedError({
+      className: CacheAdapter.name,
+      functionName: 'remove'
+    })
   }
 }
