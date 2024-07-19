722d550ee083736ca77828ae02367828e8132f59
Nicolas Melo <nicolasmelo12@gmail.com>
2024-07-19 02:13:26 -0300

    making more test to the framework, going to work on schema next

--- packages/databases/__tests__/src/test/models.ts
+++ packages/databases/__tests__/src/test/models.ts
@@ -1,10 +1,17 @@
-import { AutoField, CharField, DateField, EnumField, ForeignKeyField, IntegerField, Model, ModelOptionsType, ON_DELETE, UuidField } from '@palmares/databases';
+import { AutoField, BooleanField, CharField, DateField, DecimalField, EnumField, ForeignKeyField, IntegerField, Model, ModelOptionsType, ON_DELETE, UuidField } from '@palmares/databases';
 import { User as DUser, Company as DCompany } from '../../.drizzle/schema';
+
 export class User extends Model<User>() {
   fields = {
     id: AutoField.new(),
+    uuid: UuidField.new({
+      autoGenerate: true
+    }),
     name: CharField.new({ maxLength: 255, dbIndex: true, allowNull: true }),
     age: IntegerField.new({ dbIndex: true }),
+    userType: EnumField.new({ choices: ['admin', 'user'], defaultValue: 'admin' }),
+    price: DecimalField.new({ maxDigits: 5, decimalPlaces: 2, allowNull: true }),
+    isActive: BooleanField.new({ defaultValue: true }),
     companyId: ForeignKeyField.new({
       onDelete: ON_DELETE.CASCADE,
       relatedName: 'usersOfCompany',
@@ -26,7 +33,7 @@ export class Company extends Model<Company>() {
   fields = {
     id: AutoField.new(),
     name: CharField.new({ maxLength: 255 }),
-    address: CharField.new({ maxLength: 255 }),
+    address: CharField.new({ maxLength: 255, allowNull: true }),
   }
 
   options: ModelOptionsType<Company> = {
--- packages/databases/__tests__/src/test/test.test.ts
+++ packages/databases/__tests__/src/test/test.test.ts
@@ -5,12 +5,40 @@ import JestTestAdapter from '@palmares/jest-tests';
 
 import { Company, User } from './models';
 
-describe<JestTestAdapter>('first test', ({ test }) => {
-  test('test', async ({ expect }) => {
+describe<JestTestAdapter>('models', ({ test }) => {
+  test('its assigning a different date', async ({ expect }) => {
     const company = await Company.default.set({ name: 'test', address: 'test' });
-    const user = await User.default.set({ name: 'test', companyId: company[0].id, age: 10 });
-
+    const user = await User.default.set({ name: 'test', companyId: company[0].id, age: 10 } as any);
+    const createdAtDate = user[0].createdAt;
+    const previousUpdatedAt = user[0].updatedAt;
     await new Promise((resolve) => setTimeout(resolve, 2000));
-    await User.default.set({ name: 'test' }, { search: { id: user[0].id}});
+    const afterUpdate = await User.default.set({ name: 'test' }, { search: { id: user[0].id}});
+    const newCreatedAtDate = user[0].createdAt;
+    const newUpdatedAtDate = afterUpdate[0].updatedAt;
+
+    expect(newUpdatedAtDate).not.toBe(createdAtDate);
+    expect(newUpdatedAtDate > previousUpdatedAt).toBe(true);
+    expect(newCreatedAtDate).toBe(newCreatedAtDate);
+  });
+
+  test('its not allowing null to non nullable fields', async ({ expect }) => {
+    try {
+      await Company.default.set({ name: null } as any);
+    } catch (e) {
+      expect(true).toBe(true);
+    }
+  });
+
+  test('its allowing null to nullable fields', async ({ expect }) => {
+    const data = await Company.default.set({ name: 'test', address: null });
+    expect(data[0].address).toBe(null);
+  });
+
+  test('its not allowing null to non nullable fields', async ({ expect }) => {
+    try {
+      await User.default.set({ name: null } as any);
+    } catch (e) {
+      expect(true).toBe(true);
+    }
   });
 });
