ff061250fcebe52758f611567ce2e8afebdcbe09
Nicolas Melo <nicolasmelo12@gmail.com>
2024-11-11 21:14:16 -0300

    remove templates from bin, let it be defined from each package and add it to gitignore

--- bin/templates/db/drizzle/migrate.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line ts/ban-ts-comment
-// @ts-nocheck
-import { migrate } from '@palmares/drizzle-engine/better-sqlite3/migrator';
-
-import { db } from './database.config';
-
-migrate(db, { migrationsFolder: './.drizzle/migrations' });
