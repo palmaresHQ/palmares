853816015cce37cc77a2ce0e85237285accca5c3
Nicolas Melo <nicolasmelo12@gmail.com>
2025-02-12 14:19:35 -0300

    change how we fetch the monaco worker

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -4,7 +4,15 @@ import monacoWorkerPlugin from './plugins/vite-worker-plugin';
 export default defineConfig({
   server: {
     preset: 'vercel',
-    plugins: ['./plugins/nitro-worker.plugin.ts']
+    plugins: ['./plugins/nitro-worker.plugin.ts'],
+    routeRules: {
+      '/assets/**': {
+        headers: {
+          'Cross-Origin-Embedder-Policy': 'require-corp',
+          'Cross-Origin-Opener-Policy': 'same-origin'
+        }
+      }
+    }
   },
   vite: {
     plugins: [monacoWorkerPlugin() as any],
