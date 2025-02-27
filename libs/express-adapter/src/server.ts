68515ac00776fa5f866b410c08bc34fd759e188f
brenoliradev <brenoliradev@gmail.com>
2025-02-27 15:04:02 -0300

    feat: implement method 'new' to ExpressServer

--- libs/express-adapter/src/server.ts
+++ libs/express-adapter/src/server.ts
@@ -1,4 +1,4 @@
-import { serverAdapter } from '@palmares/server';
+import { AllServerSettingsType, ServerAdapter, serverAdapter, ServerRouterAdapter, ServerSettingsType } from '@palmares/server';
 import express, { type Express } from 'express';
 
 import { requestAdapter } from './request';
@@ -8,6 +8,7 @@ import { routerAdapter } from './router';
 import type { ServerSettingsTypeExpress } from './types';
 import type { Domain } from '@palmares/core';
 import type multer from 'multer';
+import { ExpressRequestAdapter } from '.';
 
 export const servers = new Map<
   string,
@@ -54,5 +55,50 @@ const expressServerAdapter = serverAdapter({
   }
 });
 
-export { expressServerAdapter as ExpressServerAdapter };
-export { expressServerAdapter as default };
+let defaultConfig = {} as ServerSettingsTypeExpress;
+
+class ExpressServerAdapter extends ServerAdapter {
+  $$type = '$PServerAdapter';
+  request = new requestAdapter();
+  response = new responseAdapter();
+  routers = new routerAdapter();
+  
+  // eslint-disable-next-line ts/require-await
+  async load(serverName: string, _domains: Domain[], settings: ServerSettingsTypeExpress) {
+    let server: Express | undefined = servers.get(serverName)?.server;
+    if (!server) {
+      server = express();
+      servers.set(serverName, { server, settings });
+    }
+    if (settings.customServerSettings?.middlewares) {
+      settings.customServerSettings.middlewares.forEach((middleware) => {
+        server.use(middleware);
+      });
+    }
+    return server;
+  }
+  
+  // eslint-disable-next-line ts/require-await
+  static close(serverName: string) {
+    servers.get(serverName)?.httpServer?.close();
+    servers.delete(serverName);
+  }
+
+  // eslint-disable-next-line ts/require-await
+  async start(_serverName: string, server: Express, port: number, logServerStart: () => void) {
+    server.listen(port, () => logServerStart());
+  }
+
+  static new(args: Omit<ServerSettingsTypeExpress, 'server'>) {
+    defaultConfig = {
+      server: this,
+      ...args,
+    };
+
+    return {
+      server: this,
+    };
+  }
+}
+
+export { ExpressServerAdapter, expressServerAdapter };
