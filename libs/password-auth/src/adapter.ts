6101cdccd8c9e1c8c8311625c94071443c8559f4
Vicentesan <vikom.sanchez@gmail.com>
2025-03-22 23:50:09 -0300

    feat: add salt rounds config to password adapter

--- libs/password-auth/src/adapter.ts
+++ libs/password-auth/src/adapter.ts
@@ -1,14 +1,10 @@
 import { authAdapter } from '@palmares/auth';
 import { compare, hash } from 'bcrypt';
 
-export const passwordAdapter = authAdapter(() => ({
+export const passwordAdapter = authAdapter((config: { saltRounds?: number } = {}) => ({
   name: 'password',
   methods: {
-    hash: (password: string) => {
-      return hash(password, 10);
-    },
-    validate: (password: string, toValidate: string) => {
-      return compare(password, toValidate);
-    }
+    hash: (password: string) => hash(password, config.saltRounds ?? 10),
+    validate: (password: string, toValidate: string) => compare(password, toValidate)
   }
 }));
