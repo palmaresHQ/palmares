763b4bc96c75d5e03f82919c04c688f36e0cb8e4
Nicolas Melo <nicolasmelo12@gmail.com>
2025-03-31 11:11:28 -0300

    remove dependencies

--- docs/public/dependencies/drizzle-orm/0.41.0/version.d.ts
+++
@@ -1,5 +0,0 @@
-var version = "0.41.0";
-
-declare const compatibilityVersion = 10;
-
-export { compatibilityVersion, version as npmVersion };
--- docs/public/dependencies/expo/52.0.41/fetch.d.ts
+++
@@ -1,1 +0,0 @@
-export * from './build/winter/fetch/index';
